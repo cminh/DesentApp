48cb8c4809fb134ccfb9688b228ea816a583a703
Magnus Tangen <magnus-tangen@hotmail.com>
2017-06-23 07:59:40 +0200

    New drawer v1.0

--- app/src/main/java/com/example/desent/desent/activities/MainActivity.java
+++ app/src/main/java/com/example/desent/desent/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.desent.desent.activities;
 
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
@@ -59,6 +60,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     protected View informationCO2left;
 
+    // Drawer
+    private DrawerLayout drawer;
+    private  Toolbar toolbar;
+    private NavigationView navigationView;
+
+
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return false;
@@ -80,11 +88,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Navigation drawer
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        // Drawer "hamburger"
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //Navigation drawer
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
@@ -138,6 +149,82 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         timeSpinner.setOnItemSelectedListener(timeSpinnerActivity);
 
         goToDailyView();
+
+    }
+
+    private void setUpNavigationView() {
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.nav_home:
+
+
+                        // launch new intent instead of loading fragment
+                        /*
+                        startActivity(new Intent(MainActivity.this, AboutUsActivity.class));
+                        drawer.closeDrawers();
+                        */
+                        return true;
+                    case R.id.nav_settings:
+
+
+                        // launch new intent instead of loading fragment
+                        startActivity(new Intent(MainActivity.this, Settings.class));
+                        drawer.closeDrawers();
+                        return true;
+                    case R.id.nav_about_us:
+
+
+                        // launch new intent instead of loading fragment
+                        /*
+                        startActivity(new Intent(MainActivity.this, AboutUsActivity.class));
+                        drawer.closeDrawers();
+                        */
+                        return true;
+                    default:
+
+                }
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if (menuItem.isChecked()) {
+                    menuItem.setChecked(false);
+                } else {
+                    menuItem.setChecked(true);
+                }
+                menuItem.setChecked(true);
+
+                return true;
+            }
+        });
+
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawer.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessary or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
     }
 
     AdapterView.OnItemSelectedListener timeSpinnerActivity = new AdapterView.OnItemSelectedListener() {
@@ -433,5 +520,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         indicatorsBarFragment.setUp();
 
         updateCO2left();
+
+        // Drawer setup
+        setUpNavigationView();
     }
 }
