ee9121c92f8441f571ec5e3eb593f66248c892bd
magnus private <magnus-tangen@hotmail.com>
2018-04-23 00:17:20 +0200

    Data processing 1.3

--- app/src/main/java/com/example/desent/desent/models/DistanceTracker.java
+++ app/src/main/java/com/example/desent/desent/models/DistanceTracker.java
@@ -312,10 +312,10 @@ public class DistanceTracker extends MainActivity implements SensorEventListener
                 if(isInserted == true){
                     String distToast = "Walking: " + String.format("%.1f", myDb.getWalkingDistanceToday()) + " Cycling: " + String.format("%.1f", myDb.getCyclingDistanceToday()) + " Driving: " +  String.format("%.1f", myDb.getDrivingDistanceToday());
                     Log.i(TAG, "Data inserted. " + distToast);
-                    Toast.makeText(context,"Data inserted. " + distToast,Toast.LENGTH_LONG).show();
+                    //Toast.makeText(context,"Data inserted. " + distToast,Toast.LENGTH_LONG).show();
                 }
                 else{
-                    Toast.makeText(context,"Data not inserted",Toast.LENGTH_LONG).show();
+                    //Toast.makeText(context,"Data not inserted",Toast.LENGTH_LONG).show();
                 }
             }
         }
